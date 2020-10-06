49e984f25d1954ed040b6f2cb3114e94808d4479
Dmitry <dimitriy@captureproof.com>
2020-10-06 22:57:03 +0300

    main page

---
+++ src/components/main_page.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>Main page</h1>
+    <router-link to="/sign-up">SignUp</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {},
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignUp from '../components/sign_up/new.vue'
+import MainPage from '../components/main_page.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-    // {
-    //     path: '/',
-    //     name: 'Home',
-    //     component: HomePage
-    // },
+    {
+        path: '/',
+        name: 'MainPage',
+        component: MainPage
+    },
     {
         path: '/sign-up',
         name: 'SignUp',
